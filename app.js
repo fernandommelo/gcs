ce04dbfd3fbb8f45cf6d6d853575eb00f8fab145
fernandommelo <mdmelo.fernando@gmail.com>
2025-06-09 19:40:02 -0400

    feat(avalia_circunferencia): formulário e lógica de avaliação implementados

--- app.js
+++ app.js
@@ -14,5 +14,25 @@ APP.listen(8080, (error) => {
 });
 
 APP.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {message : null});
+});
+
+APP.post('/avaliarCircunferencia', (req, res) => {
+    let circunferencia = parseFloat(req.body.circunferencia);
+    let sexo = req.body.sexo;
+
+    let message;
+    
+    if(!circunferencia || !sexo) {
+        message = 'Preencha todos os valores'
+        return res.render('index', { message });   
+    }
+
+    if((sexo == 'masculino' && circunferencia > 94) || (sexo == 'feminino' && circunferencia > 80)) {
+        message = 'Possui riscos! Consultar médico'
+        return res.render('index', { message });   
+    }
+
+    message = 'Fora de risco!' 
+    res.render('index', { message });
 });
